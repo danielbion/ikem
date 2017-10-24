b4f28d823f290b8e91412d8d501414eae55e01b9
Daniel Bion Barreiros <daniel.barreiros@manifestogames.com.br>
2017-10-24 02:18:09 -0300

    - Starting IKEM_IS with IEM, generating results on real datasets

--- IKEM_IS.r
+++ IKEM_IS.r
@@ -23,148 +23,7 @@ IKEMIS = function(x, repPerIteration, D, plot, iterations){
 		}		
 		soma = soma^0.5
 	}
-	
-	inicializarPosteriori = function(){		
-		guess=sample(5:(n-5),2,replace=FALSE)
 		
-		if (guess[1] < guess[2]){
-			guess[1]=guess[1] - 4
-			guess[2]=guess[2] + 4
-		}else{
-			guess[2]=guess[2] - 4
-			guess[1]=guess[1] + 4
-		}
-		
-		#guess = c(37,47)
-		#print(guess)
-		#Escolher duas observações aleatoriamente
-		g1 = x[guess[1],]       
-		g2 = x[guess[2],]
-
-		alvo = rep(0, n)
-
-		#Definir o alvo inicial baseado na distância entre os dados e as duas observações escolhidas
-		for(i in 1:n){
-			d1 = 0
-			d2 = 0
-			for(j in 1:pp){
-				d1 = d1 + (x[i,j] - g1[j])^2
-				d2 = d2 + (x[i,j] - g2[j])^2
-			}			
-			if (d1 < d2){
-				alvo[i]=1 
-			}else{ 
-				alvo[i]=2 
-			}
-		}
-		
-		infSeq =  seq(from = 1, to = pp, by = 2)
-		
-		H = c()		
-		for(j in infSeq){
-			soma = 0
-			for(i in 1:n){
-				for(h in 1:n){
-					difInf = abs(x[i, j] - x[h, j])
-					difSup = abs(x[i, j+1] - x[h, j+1])
-					maxDif = max(difInf, difSup)
-					soma = soma + maxDif^2					
-				}
-			}
-			soma = soma/(2*(n^2))
-			H[j] = soma^0.5
-		}
-		
-		for(i in 1:n){
-			d1 = hausDist(x[i,], g1, H)
-			d2 = hausDist(x[i,], g2, H)
-			
-			if (d1 < d2){
-				alvo[i]=1 
-			}else{ 
-				alvo[i]=2 
-			}
-		}
-		
-		theta = list(
-			tau=c(0.5,0.5),
-			media = matrix(			 
-			  c(apply(x[alvo==1,1:pp],2,mean),
-			    apply(x[alvo==2,1:pp],2,mean)),
-			    nrow = c, ncol = pp, byrow=TRUE),	
-			mcov = list(matrix(0,nrow=p,ncol=p,byrow=TRUE),
-						matrix(0,nrow=p,ncol=p,byrow=TRUE))
-		)		
-				
-		mcovA = matrix(0,nrow=p,ncol=p,byrow=TRUE)
-		mcovB = matrix(0,nrow=p,ncol=p,byrow=TRUE)
-		for(i in 1:p){
-			infIdxi = (i*2) - 1
-			supIdxi = (i*2)
-			for(j in 1:p){
-				infIdxj = (j*2) - 1
-				supIdxj = (j*2)
-				mcovA[i,j] = (cov(x[alvo==1, infIdxi], x[alvo==1, infIdxj]) + cov(x[alvo==1, supIdxi], x[alvo==1, supIdxj]))/2 
-				mcovB[i,j] = (cov(x[alvo==2, infIdxi], x[alvo==2, infIdxj]) + cov(x[alvo==2, supIdxi], x[alvo==2, supIdxj]))/2 
-			}			
-		}		
-				
-		tipoMatriz = 3
-		if(tipoMatriz == 1){
-			# Matriz Identidade
-			mcovA = diag(p)
-			mcovB = diag(p)			
-		}else if(tipoMatriz == 2){
-			# Matriz diagonal
-			for(i in 1:p){				
-				for(j in 1:p){				
-					if(i != j){
-						mcovA[i,j] = 0
-						mcovB[i,j] = 0
-					}
-				}
-			}			
-		}
-		
-		theta$mcov[[1]] = mcovA
-		theta$mcov[[2]] = mcovB
-					
-		for (i in 1:n)
-		{
-			A = c()
-			B = c()						
-			
-			for(k in 1:c){
-				XMinf = c()
-				XMsup = c()
-				for(j in 1:p){
-					infIdx = (2*j) - 1
-					supIdx = 2*j
-					XMinf = cbind(XMinf, x[i,infIdx] - theta$media[k,infIdx])	
-					XMsup = cbind(XMsup, x[i,supIdx] - theta$media[k,supIdx])	
-				}
-				A[k] = XMinf %*% solve(theta$mcov[[k]]) %*% t(XMinf)
-				B[k] = XMsup %*% solve(theta$mcov[[k]]) %*% t(XMsup)
-				#A[k] = XMinf %*% ginv(theta$mcov[[k]]) %*% t(XMinf)
-				#B[k] = XMsup %*% ginv(theta$mcov[[k]]) %*% t(XMsup)
-			}			
-					
-			numA = theta$tau[1] * (exp(-1/2 * (A[1]+B[1])/2) / sqrt(((2*pi)^p) * det(theta$mcov[[1]])))	
-			numB = theta$tau[2] * (exp(-1/2 * (A[2]+B[2])/2) / sqrt(((2*pi)^p) * det(theta$mcov[[2]])))
-			den = numA + numB
-			
-			if(is.nan(den)){
-				print('DEN == 0')
-			}
-			
-			posteriori[i, 1] = numA/den
-			posteriori[i, 2] = numB/den			
-		}
-		retorno = list(posteriori, theta)
-		names(retorno) = c('Posteriori','Theta')
-		return(retorno)			
-	}
-	
 	E.step = function(){				
 		for (i in 1:n)
 		{
@@ -186,7 +45,8 @@ IKEMIS = function(x, repPerIteration, D, plot, iterations){
 				B[k] = XMsup %*% solve(theta$mcov[[k]]) %*% t(XMsup)
 				#A[k] = XMinf %*% ginv(theta$mcov[[k]]) %*% t(XMinf)
 				#B[k] = XMsup %*% ginv(theta$mcov[[k]]) %*% t(XMsup)
-				num = cbind(num, theta$tau[k] * (exp(-1/2 * (A[k]+B[k])/2) / sqrt(((2*pi)^p) * det(theta$mcov[[k]]))))				
+				fdp = normal(A[k], B[k], theta$mcov[[k]])
+				num = cbind(num, theta$tau[k] * fdp)				
 			}	
 
 			den = sum(num)
@@ -198,6 +58,13 @@ IKEMIS = function(x, repPerIteration, D, plot, iterations){
 			posteriori[i, ] = num/den			
 		}
 		return(posteriori)
+	}
+	
+	normal = function(A, B, Sigma){
+		num = exp(-0.5 * (A + B) * 0.5)
+		den1 = ((2 * pi)^p)^0.5
+		den2 = det(Sigma)
+		return (num/(den1 * den2))
 	}
 
 	M.step = function(){	
@@ -272,20 +139,14 @@ IKEMIS = function(x, repPerIteration, D, plot, iterations){
 		
 		melhorML = 10000000000;  
 		
-		for(ii in 1:iterations){
-			tau = c(0.5, 0.5)			
+		for(ii in 1:iterations){		
 			t = 0	
 			MLatual = 1	
 			MLdepois = -1
-			# kernelInicial = inicializarK()
 
-			posteriori = matrix(0.5, nrow = n, ncol = c)					
-			retorno = inicializarPosteriori()
+			retorno = IEM(x, tipoMatriz = 3, numRep = 1, plot = FALSE)
 			posteriori = retorno$Posteriori
-			#posteriori[,1] = runif(n, 0, 1)
-			#posteriori[,2] = 1 - posteriori[,1]
 			theta = retorno$Theta
-			
 			old_posteriori = posteriori
 						
 			while(t<repPerIteration && abs(MLatual - MLdepois)>epsilon){
